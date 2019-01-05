f4a4a974ee3a13440b1089ca867d5b014d13a17d
Victor Chen <vichen@zynga.com>
2019-01-05 15:05:25 -0500

    Initial commit

---
+++ client/src/App.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import OtherPage from './OtherPage';
+import Fib from './Fib';
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div className="App">
+          <header>
+            <Link to="/">Home</Link>&nbsp;
+            <Link to="/otherpage">Other Page</Link>
+          </header>
+          <div>
+            <Route exact path="/" component={Fib}></Route>
+            <Route path="/otherpage" component={OtherPage}></Route>
+          </div>
+        </div>
+      </Router>
+    );
+  }
+}
+
+export default App;
